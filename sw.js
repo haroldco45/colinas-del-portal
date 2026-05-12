9b9be8ca814ed5e38fb1fdcca8340e789dd1c64d
haroldco45 <68300211+haroldco45@users.noreply.github.com>
2026-05-12 17:04:57 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,10 @@
-const CACHE_NAME = 'colinas-portal-v1';
-const ASSETS = [
-  'index.html',
-  'manifest.json'
-];
+const CACHE_NAME = 'colinas-portal-v2';
+const ASSETS = ['index.html', 'manifest.json'];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
 });
